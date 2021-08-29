88374bc8ff25021963dafe1d63515ac48c490232
Pimkhwan <kanyawe34854@gmail.com>
2021-08-29 22:53:00 +0700

    commit

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Pimkhwan Kamendee 640612187</p>
+        </div>
+    )
+}
+
+export default About
